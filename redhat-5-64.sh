8b8b76e254f71eb2a4afa1f7c10a543bcb86f0f8
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-06 17:19:55 -0500

    Import EPEL metadata RPMs instead of trying to download at build time

--- redhat-5-64.sh
+++ redhat-5-64.sh
@@ -2,8 +2,6 @@
 
 . ./redhat-5.sh
 
-wget http://mirror.itc.virginia.edu/5/i386/epel-release-5-4.noarch.rpm
-
 rpm --install epel-release-5-4.noarch.rpm
 
 yum -y groupinstall "Development Tools"
--- redhat-6-64.sh
+++ redhat-6-64.sh
@@ -2,8 +2,6 @@
 
 . ./redhat-6.sh
 
-wget http://mirror.itc.virginia.edu/6/i386/epel-release-6-8.noarch.rpm
-
 rpm --install epel-release-6-8.noarch.rpm
 
 yum --assumeyes groupinstall "Development Tools"
