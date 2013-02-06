68ee4a754c6d60dd1fca60a4aa3a18144a8628e8
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-06 16:05:08 -0500

    Red Hat 5 version of 'yum' does not understand '--assumeyes'

--- redhat-5-64.sh
+++ redhat-5-64.sh
@@ -6,6 +6,6 @@ wget http://mirror.itc.virginia.edu/5/i386/epel-release-5-4.noarch.rpm
 
 rpm --install epel-release-5-4.noarch.rpm
 
-yum --assumeyes groupinstall "Development Tools"
+yum -y groupinstall "Development Tools"
 
-yum --assumeyes install git-core ccache glibc-devel.i386 libstdc++.i386
+yum -y install git-core ccache glibc-devel.i386 libstdc++.i386
--- redhat-5.sh
+++ redhat-5.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ex
 
-yum --assumeyes update
+yum -y update
 
-yum --assumeyes remove java-1.4.2-gcj-compat
+yum -y remove java-1.4.2-gcj-compat
 
-yum --assumeyes install java-1.6.0-openjdk
+yum -y install java-1.6.0-openjdk
 
 mkdir /jenkins
