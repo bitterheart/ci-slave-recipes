84fce914674c13ebc95fc928432b108d64661d46
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-07 14:21:28 -0500

    Move zypper global option to the proper place in the command line

--- opensuse-12-64.sh
+++ opensuse-12-64.sh
@@ -4,7 +4,7 @@
 
 zypper addrepo --check --refresh http://download.opensuse.org/repositories/devel:/tools/openSUSE_12.2/devel:tools.repo
 
-zypper install --no-confirm --gpg-auto-import-keys git-core ccache gcc-c++ sloccount valgrind cppcheck llvm-clang gcc47-32bit libstdc++47-devel-32bit
+zypper --gpg-auto-import-keys install --no-confirm git-core ccache gcc-c++ sloccount valgrind cppcheck llvm-clang gcc47-32bit libstdc++47-devel-32bit
 
 # Need ccache symlinks?
 
