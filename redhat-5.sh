dfe7a36433ce6d97a7c5993fbe78b9b8abf9ec78
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-07 11:00:07 -0500

    Document why special remove command is needed

--- redhat-5.sh
+++ redhat-5.sh
@@ -2,6 +2,9 @@
 
 yum -y update
 
+# this must be removed before java-1.4.2-gcj-compat due to a bug in 'yum'
+# that will cause the script to fail if it is automatically removed due to
+# its dependency on that package
 yum -y remove postgresql-jdbc
 
 yum -y remove java-1.4.2-gcj-compat
