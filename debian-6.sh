3a74de84435d249639201f9e7582b036eb9c6385
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-11 15:29:38 -0500

    All systems need Python, not just builders

--- debian-6.sh
+++ debian-6.sh
@@ -4,7 +4,7 @@ apt-get --quiet --assume-yes update
 
 apt-get --quiet --assume-yes dist-upgrade
 
-apt-get --quiet --assume-yes install openjdk-6-jre
+apt-get --quiet --assume-yes install openjdk-6-jre python
 
 mkdir /jenkins
 chown admin: /jenkins
