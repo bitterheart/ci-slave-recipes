f22854b50b4e34680f895b3eeb41fb320a49e4c7
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-11 11:48:43 -0500

    Try to work around openSUSE 12 update problem

--- opensuse-12.sh
+++ opensuse-12.sh
@@ -2,6 +2,10 @@
 
 zypper modifyrepo --disable Virtualization:Cloud:EC2_12.2
 
+zypper refresh
+
+zypper update --no-confirm xen-tools-domU
+
 zypper update --no-confirm
 
 zypper install --no-confirm java-1_7_0-openjdk
--- suse-11.sh
+++ suse-11.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 
+zypper refresh
+
 zypper update --no-confirm
 
 zypper install --no-confirm java-1_6_0-ibm
