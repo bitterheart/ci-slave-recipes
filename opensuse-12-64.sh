98f029dd1f6eccf048fed1beea0abde5f5f4bb7a
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-02-15 09:24:25 -0500

    Install CA certificates so GitHub can be contacted over HTTPS

--- opensuse-12-64.sh
+++ opensuse-12-64.sh
@@ -5,5 +5,6 @@
 zypper addrepo --check --refresh http://download.opensuse.org/repositories/devel:/tools/openSUSE_12.2/devel:tools.repo
 zypper addrepo --check --refresh http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_12.2/devel:tools:scm.repo
 
-zypper --gpg-auto-import-keys install --no-confirm git-core ccache gcc-c++ sloccount valgrind cppcheck llvm-clang gcc47-32bit libstdc++47-devel-32bit
+zypper --gpg-auto-import-keys install --no-confirm git-core ccache gcc-c++ sloccount valgrind cppcheck llvm-clang gcc47-32bit libstdc++47-devel-32bit ca-certificates-mozilla
+
 
