58f0a4f9cf8fc03e20f5fc20e2c0929237e21020
Kevin P. Fleming <kevin@kpfleming.us>
2013-03-14 14:55:21 -0400

    Modify scripts to do distribution-upgrade to version 12.3 for openSUSE

--- opensuse-12.sh
+++ opensuse-12.sh
@@ -1,12 +1,17 @@
 #!/bin/sh -ex
 
-zypper modifyrepo --disable Virtualization:Cloud:EC2_12.2
+# remove openSUSE 12.2 repositories
+zypper removerepo Virtualization:Cloud:EC2_12.2
+zypper removerepo openSUSE_12.2_OSS
+zypper removerepo openSUSE_12.2_Updates
+
+# add openSUSE 12.3 repositories
+zypper addrepo --refresh http://download.opensuse.org/distribution/12.3/repo/oss openSUSE_12.3_OSS
+zypper addrepo --refresh http://download.opensuse.org/update/12.3 openSUSE_12.3_Updates
 
 zypper refresh
 
-zypper update --no-confirm xen-tools-domU
-
-zypper update --no-confirm
+zypper dup
 
 zypper install --no-confirm java-1_7_0-openjdk
 
