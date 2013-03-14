37201eaab6490d52070d5688b6ca0a748ff78df3
Kevin P. Fleming <kevin@kpfleming.us>
2013-03-14 16:17:34 -0400

    Ensure that correct repository is enabled

--- opensuse-12-64.sh
+++ opensuse-12-64.sh
@@ -2,7 +2,6 @@
 
 . ./opensuse-12.sh
 
-# zypper --non-interactive addrepo --check --refresh http://download.opensuse.org/repositories/devel:/tools/openSUSE_12.3/devel:tools.repo
-zypper --non-interactive addrepo --check --refresh http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_12.3/devel:tools:scm.repo
+zypper --non-interactive addrepo --check --refresh http://download.opensuse.org/repositories/devel:/tools/openSUSE_12.3/devel:tools.repo
 
 zypper --non-interactive --gpg-auto-import-keys install --auto-agree-with-licenses git-core ccache gcc-c++ sloccount valgrind cppcheck llvm-clang gcc47-32bit libstdc++47-devel-32bit
