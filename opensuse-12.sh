76da1e702a06c8773611216b2ab6036b313ed4fc
Kevin P. Fleming <kevin@kpfleming.us>
2013-03-14 15:41:23 -0400

    Fix various script bugs

--- opensuse-12.sh
+++ opensuse-12.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -ex
 
 # remove openSUSE 12.2 repositories
-zypper removerepo Virtualization:Cloud:EC2_12.2
-zypper removerepo openSUSE_12.2_OSS
-zypper removerepo openSUSE_12.2_Updates
+zypper --non-interactive removerepo Virtualization:Cloud:EC2_12.2
+zypper --non-interactive removerepo openSUSE_12.2_OSS
+zypper --non-interactive removerepo openSUSE_12.2_Updates
 
 # add openSUSE 12.3 repositories
-zypper addrepo --refresh http://download.opensuse.org/distribution/12.3/repo/oss openSUSE_12.3_OSS
-zypper addrepo --refresh http://download.opensuse.org/update/12.3 openSUSE_12.3_Updates
+zypper --non-interactive addrepo --refresh http://download.opensuse.org/distribution/12.3/repo/oss openSUSE_12.3_OSS
+zypper --non-interactive addrepo --refresh http://download.opensuse.org/update/12.3 openSUSE_12.3_Updates
 
-zypper refresh
+zypper --non-interactive refresh
 
-zypper dup
+zypper --non-interactive dist-upgrade --auto-agree-with-licenses
 
-zypper install --no-confirm java-1_7_0-openjdk
+zypper --non-interactive install --auto-agree-with-licenses java-1_7_0-openjdk
 
 mkdir /jenkins
