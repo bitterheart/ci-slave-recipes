c171e59f66a5339b4106b6175da115864bbc5da9
Kevin P. Fleming <kpfleming@bloomberg.net>
2013-05-06 09:05:50 -0400

    Setup for Debian 7 machines

---
+++ debian-7.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get --quiet --assume-yes update
+
+apt-get --quiet --assume-yes dist-upgrade
+
+apt-get --quiet --assume-yes install openjdk-7-jre python
+
+mkdir /jenkins
+chown admin: /jenkins
